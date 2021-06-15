bf41f97e50abad4a17e754a864dff3ab339eb6b3
Callum Hyland <hello@callumhyland.com>
2021-06-15 16:05:52 +0100

    update technologies

--- src/App.svelte
+++ src/App.svelte
@@ -39,24 +39,30 @@
   <h1>🧙‍♂️ Skills</h1>
 
   <h2>Languages</h2>
-  <img src="images/typescript.svg" alt="TypeScript" />
   <img src="images/javascript.svg" alt="JavaScript" />
+  <img src="images/typescript.svg" alt="TypeScript" />
   <img src="images/python.svg" alt="Python" />
 
   <h2>Frameworks</h2>
   <img src="images/react.svg" alt="React" />
   <img src="images/svelte.svg" alt="Svelte" />
-
   <img src="images/graphql.png" alt="GraphQL" />
   <img src="images/nodejs.svg" alt="NodeJS" />
+
+  <h2>Infrastructure</h2>
+  <img src="images/aws.svg" alt="AWS" />
+  <img src="images/terraform.png" alt="Terraform" />
+  <img src="images/kube.svg" alt="Kubernetes" />
+  <img src="images/docker.png" alt="Docker" />
   <img src="images/lambda.svg" alt="Lambda" />
 
   <h2>Technologies</h2>
-  <img src="images/docker.png" alt="Docker" />
   <img src="images/android.svg" alt="Android" />
   <img src="images/ios.svg" alt="iOS" />
   <img src="images/ethereum.png" alt="Ethereum" />
+  <img src="images/polkadot.svg" alt="Polkadot" />
   <img src="images/unity.png" alt="Unity" />
+  <img src="images/unreal.svg" alt="Unreal" />
 
   <!-- Education -->
   <h1>🎓 Education</h1>
